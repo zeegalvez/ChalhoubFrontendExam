16adf049e2da713aabccb01adf456de539ab71be
Zee Galvez <zelmar.b.galvez@gmail.com>
2021-03-14 13:36:12 +0800

    FE: test data

---
+++ src/testData/nav.js
@@ -0,0 +1,30 @@
+// Sample menu
+const navMenu = {
+  menu1: {
+    name: "Men",
+    url: "",
+    status: "active"
+  },
+  menu2: {
+    name: "Women",
+    url: "",
+    status: "active"
+  },
+  menu3: {
+    name: "Kids",
+    url: "",
+    status: "active"
+  },
+  menu4: {
+    name: "FTW",
+    url: "",
+    status: "active"
+  },
+  menu5: {
+    name: "Accessories",
+    url: "",
+    status: "active"
+  },
+};
+
+export default navMenu;
