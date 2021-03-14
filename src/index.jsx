2e35a3b95ee32ff2b0316e54ee5c2385226f886e
Zee Galvez <zelmar.b.galvez@gmail.com>
2021-03-14 13:34:45 +0800

    FE: initial commit - react

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+
+import $ from 'jquery';
+import style from '../styles/style.scss';
+import 'bootstrap/dist/js/bootstrap.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+const root = document.getElementById('react-app');
+ReactDOM.render(<App />, root);
